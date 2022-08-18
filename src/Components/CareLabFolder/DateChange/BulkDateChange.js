ba79cd062d8954fb37a9026a59d7348e338705f5
Prabal Rai <Prabalrai17@gmail.com>
2022-08-18 17:06:43 +0545

    date change ui options added

--- src/Components/CareLabFolder/DateChange/BulkDateChange.js
+++ src/Components/CareLabFolder/DateChange/BulkDateChange.js
@@ -1,13 +1,51 @@
+import { Table } from "antd";
 import React from "react";
 import Filter from "../../Common/Filter";
 import PageHeader from "../../Common/pageHeader";
 
 function BulkDateChange() {
+  const columns = [
+    {
+      title: "SN",
+    },
+    {
+      title: "Name",
+    },
+    {
+      title: "Contact No",
+    },
+    {
+      title: "Collection Date",
+    },
+    {
+      title: "Nep. Collection Date",
+    },
+    {
+      title: "Collection Action",
+    },
+    {
+      title: "Result Date",
+    },
+    {
+      title: "Nep.Result Date",
+    },
+    {
+      title: "Result Action",
+    },
+  ];
   return (
     <>
       <div className="maiTopContainer">
         <PageHeader pageTitle={"Bulk Date Change"} />
-        <Filter dateRange serchButton forTestStatus />
+        <Filter dateChangeSampleIdFromTo getFiscalYear serchButton />
+      </div>
+      <div className="financeCards">
+        <h3>Patient Details</h3>
+        <Filter onSearch />
+
+        <div className="tableisRes">
+          <Table className="tableWidth" columns={columns} />
+        </div>
       </div>
     </>
   );
--- src/Components/CareLabFolder/DateChange/RequestorWiseDateChange.js
+++ src/Components/CareLabFolder/DateChange/RequestorWiseDateChange.js
@@ -1,13 +1,57 @@
+import { Table } from "antd";
 import React from "react";
 import Filter from "../../Common/Filter";
 import PageHeader from "../../Common/pageHeader";
 
 function RequestorWiseDateChange() {
+  const columns = [
+    {
+      title: "SN",
+    },
+    {
+      title: "Name",
+    },
+    {
+      title: "Contact No",
+    },
+    {
+      title: "Collection Date",
+    },
+    {
+      title: "Nep. Collection Date",
+    },
+    {
+      title: "Collection Action",
+    },
+    {
+      title: "Result Date",
+    },
+    {
+      title: "Nep.Result Date",
+    },
+    {
+      title: "Result Action",
+    },
+  ];
+
   return (
     <>
       <div className="maiTopContainer">
         <PageHeader pageTitle={"Requestor Wise Date Change"} />
-        <Filter dateRange serchButton forTestStatus />
+        <Filter
+          dateChangeSampleIdFromTo
+          getFiscalYear
+          getRequestor
+          serchButton
+        />
+      </div>
+      <div className="financeCards">
+        <h3>Patient Details</h3>
+        <Filter onSearch />
+
+        <div className="tableisRes">
+          <Table className="tableWidth" columns={columns} />
+        </div>
       </div>
     </>
   );
