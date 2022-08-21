013f536d220e679c8e2376727a6af090f32e2173
Saurav Tandukar <sauravtand2375@gmai.com>
2022-08-21 12:50:46 +0545

    adding minors

--- src/Components/CareLabFolder/BulkNegative/BulkNegative.js
+++ src/Components/CareLabFolder/BulkNegative/BulkNegative.js
@@ -1,10 +1,31 @@
-import { Table, Row, Col } from "antd";
+import { Row, Col, Button, Form, Table } from "antd";
+import { DatePicker, TimePicker } from "antd";
+
 import React from "react";
 import CarelabFilter from "../../Common/CarelabFilter";
+import Filter from "../../Common/Filter";
 
 import PageHeader from "../../Common/pageHeader";
 
 function BulkNegative() {
+  const columns = [
+    {
+      title: "SN",
+    },
+    {
+      title: "Patient Name",
+    },
+    {
+      title: "Contact No",
+    },
+    {
+      title: "Collection Date",
+    },
+  ];
+
+  const onFinish = (values) => {
+    console.log("Updated:", values);
+  };
   return (
     <>
       <div className="maiTopContainer">
@@ -13,21 +34,37 @@ function BulkNegative() {
         <CarelabFilter showSampleIdFromTo fiscalService />
       </div>
 
-      <div className="tableisRes">
-        <div className="financeCards">
-          <h3>Patient Details</h3>
-          <Row justify="space-between" style={{ listStyleType: "none" }}>
-            <Col>
-              <li>Sample no: </li>
-              <li>Name: </li>
-              <li>Test Lists: </li>
-            </Col>
-            <Col style={{ marginRight: "50px" }}>
-              <li>Gender: </li>
-              <li>Age: </li>
-              <li>Contact No: </li>
-            </Col>
-          </Row>
+      <div className="financeCards">
+        <h3>Patient Details</h3>
+        <Row style={{ listStyleType: "none" }}>
+          <Col style={{ marginRight: "20px" }}>
+            <label htmlFor=" " style={{ fontWeight: "bold" }}>
+              Result Date
+            </label>
+            <br />
+            <DatePicker />
+          </Col>
+          <Col style={{ marginRight: "50px" }}>
+            <label htmlFor=" " style={{ fontWeight: "bold" }}>
+              Result Time
+            </label>
+            <br />
+            <TimePicker />
+          </Col>
+
+          <Col style={{ marginTop: "20px" }}>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" onClick={onFinish}>
+                Update
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+        <div style={{ justifyContent: "end" }}>
+          <Filter onSearch />
+        </div>
+        <div className="tableisRes">
+          <Table className="tableWidth" columns={columns} />
         </div>
       </div>
     </>
--- src/Components/CareLabFolder/NotCompleted/DepartmentAnalytics.js
+++ src/Components/CareLabFolder/NotCompleted/DepartmentAnalytics.js
@@ -1,7 +1,74 @@
+import { Row, Col, Button, Form, Table } from "antd";
+import { DatePicker, TimePicker } from "antd";
+
 import React from "react";
+import CarelabFilter from "../../Common/CarelabFilter";
+import Filter from "../../Common/Filter";
+
+import PageHeader from "../../Common/pageHeader";
 
 function DepartmentAnalytics() {
-  return <h1>Department Analytics</h1>;
+  const columns = [
+    {
+      title: "SN",
+    },
+    {
+      title: "Patient Name",
+    },
+    {
+      title: "Contact No",
+    },
+    {
+      title: "Collection Date",
+    },
+  ];
+
+  const onFinish = (values) => {
+    console.log("Updated:", values);
+  };
+  return (
+    <>
+      <div className="maiTopContainer">
+        <PageHeader pageTitle={"Bulk Negative"} />
+
+        <CarelabFilter showSampleIdFromTo fiscalService />
+      </div>
+
+      <div className="financeCards">
+        <h3>Patient Details</h3>
+        <Row style={{ listStyleType: "none" }}>
+          <Col style={{ marginRight: "20px" }}>
+            <label htmlFor=" " style={{ fontWeight: "bold" }}>
+              Result Date
+            </label>
+            <br />
+            <DatePicker />
+          </Col>
+          <Col style={{ marginRight: "50px" }}>
+            <label htmlFor=" " style={{ fontWeight: "bold" }}>
+              Result Time
+            </label>
+            <br />
+            <TimePicker />
+          </Col>
+
+          <Col style={{ marginTop: "20px" }}>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" onClick={onFinish}>
+                Update
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+        <div style={{ justifyContent: "end" }}>
+          <Filter onSearch />
+        </div>
+        <div className="tableisRes">
+          <Table className="tableWidth" columns={columns} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default DepartmentAnalytics;
