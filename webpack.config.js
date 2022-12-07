c72f975e01174eb91d2f3a75e4431c71ba190f2f
Prabal Rai <Prabalrai17@gmail.com>
2022-12-07 12:49:12 +0545

    'changes'

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js",
-    publicPath: "/",
+    publicPath: "/lunivacrystal/",
   },
   module: {
     rules: [
