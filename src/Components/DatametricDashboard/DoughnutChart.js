e55e54a50c4eb40d0796497bcc5a88a09133d50e
Aashma Humagain <aashma_humagain@axiossoftwork.com>
2022-12-07 10:36:36 +0545

    certain changes

--- src/Components/DatametricDashboard/DoughnutChart.js
+++ src/Components/DatametricDashboard/DoughnutChart.js
@@ -61,11 +61,11 @@ const DoughnutChart = (props) => {
         <Doughnut options={options} data={dataDo} />
 
         <div className="">
-          <Row justify="space-around" gutter={[10, 10]}>
+          <Row justify="space-between" gutter={[10, 10]}>
             <Col lg={10}>
               <div>
                 <div className="refererename">
-                  <h4>Referer Name</h4>
+                  <h4 className="paymenttype">Payment Type</h4>
                 </div>
 
                 {labels.map((item) => (
@@ -119,5 +119,8 @@ const DoughnutContainer = styled.div`
         margin-bottom: 0px;
       }
     }
+  }
+  .paymenttype {
+    white-space: nowrap;
   }
 `;
