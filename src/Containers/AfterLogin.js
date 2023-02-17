5181260e316f58be82b046384cdfe9126e57eae6
Saurav Tandukar <sauravtand2375@gmai.com>
2023-02-17 12:21:54 +0545

    afterlogin css

--- src/Containers/AfterLogin.js
+++ src/Containers/AfterLogin.js
@@ -1,26 +1,25 @@
-import { Col, Row } from 'antd'
-import React, { useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
-import { useHistory } from 'react-router-dom'
-import styled from 'styled-components'
-import { AfterLoginData } from '../Data/MenuRoute'
-import imgOne from '../assets/images/logobig1.png'
-import { CARELAB_LINK } from '../constants/url'
+import { Col, Row } from "antd";
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import { AfterLoginData } from "../Data/MenuRoute";
+import imgOne from "../assets/images/logobig1.png";
+import { CARELAB_LINK } from "../constants/url";
 
 const AfterLogin = () => {
-  const history = useHistory()
+  const history = useHistory();
 
   const data = AfterLoginData;
-  const token = JSON.parse(localStorage.getItem('userData'));
-
+  const token = JSON.parse(localStorage.getItem("userData"));
 
   useEffect(() => {
-    console.log(token, 'from token');
+    console.log(token, "from token");
     if (token === null) {
-      history.push('/login');
-      return
+      history.push("/login");
+      return;
     }
-  }, [])
+  }, []);
 
   return (
     <DashbordContainer>
@@ -30,15 +29,20 @@ const AfterLogin = () => {
 
       <div className="options">
         <Row gutter={[16, 16]} wrap>
-          {data.map(e => (
+          {data.map((e) => (
             <>
               <Col sm={24} md={12} xs={12} lg={12} xl={8}>
-                <NavLink to={{
-                  pathname: e.path,
-                  state: e.statePath
-                }} key={e.name}>
-                  <div className='cButton' >
-                    <span><i className={e.icon}></i> </span>
+                <NavLink
+                  to={{
+                    pathname: e.path,
+                    state: e.statePath,
+                  }}
+                  key={e.name}
+                >
+                  <div className="cButton">
+                    <span>
+                      <i className={e.icon}></i>{" "}
+                    </span>
                     <span>{e.name}</span>
                   </div>
                 </NavLink>
@@ -46,49 +50,51 @@ const AfterLogin = () => {
             </>
           ))}
           <Col sm={24} md={12} xs={12} lg={12} xl={8}>
-            <a href={CARELAB_LINK} target={'_blank'} key={'redirectOther'}>
-              <div className='cButton' >
-                <span><i className={'icon-lab'}></i> </span>
+            <a href={CARELAB_LINK} target={"_blank"} key={"redirectOther"}>
+              <div className="cButton">
+                <span>
+                  <i className={"icon-lab"}></i>{" "}
+                </span>
                 <span>Lab</span>
               </div>
             </a>
           </Col>
         </Row>
       </div>
-
     </DashbordContainer>
-  )
-}
+  );
+};
 
-export default AfterLogin
+export default AfterLogin;
 
 const DashbordContainer = styled.div`
-height: 100vh;
-width: 100%;
-display: flex;
-/* justify-content: center; */
-align-items: center;
-flex-direction: column;
-/* background: #f9f9f9; */
+  height: 100%;
+  width: 100%;
+  margin-bottom: 50px;
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  flex-direction: column;
+  /* background: #f9f9f9; */
 
-.logo{
-  width: 500px;
-  height: auto;
-  margin: 80px 0;
+  .logo {
+    width: 500px;
+    height: auto;
+    margin: 80px 0;
 
-  img{
-    width: 100%;
-  }
+    img {
+      width: 100%;
+    }
 
-  @media(max-width: 800px) {
+    @media (max-width: 800px) {
       width: 300px;
       margin: 20px 0;
     }
-}
-.options{
-  width: 90%;
-} 
-  .cButton{
+  }
+  .options {
+    width: 90%;
+  }
+  .cButton {
     height: 120px;
     width: 100%;
     border-radius: 10px;
@@ -97,41 +103,37 @@ flex-direction: column;
     align-items: center;
     gap: 10px;
     background: #fefefe;
-    box-shadow: 0 2px 22px 0 rgba( 31, 38, 135, 0.17 );
-    backdrop-filter: blur( 4px );
-    -webkit-backdrop-filter: blur( 4px );
+    box-shadow: 0 2px 22px 0 rgba(31, 38, 135, 0.17);
+    backdrop-filter: blur(4px);
+    -webkit-backdrop-filter: blur(4px);
     border-radius: 10px;
-    span{
+    span {
       font-size: 20px;
       letter-spacing: 1.4px;
       text-transform: uppercase;
       color: var(--titleTxt);
-      i{
+      i {
         font-size: 25px;
         color: var(--primary);
       }
     }
-    
-    @media(max-width: 768px){
+
+    @media (max-width: 768px) {
       display: flex;
       flex-direction: column;
-      span{
-      font-size: 16px;
-      letter-spacing: 1.4px;
-      text-transform: uppercase;
-      margin-right: 10px;
-      i{
-        font-size: 25px;
+      span {
+        font-size: 16px;
+        letter-spacing: 1.4px;
+        text-transform: uppercase;
+        margin-right: 10px;
+        i {
+          font-size: 25px;
+        }
       }
     }
-    }
-    @media(max-width: 500px){
+    @media (max-width: 500px) {
       // height: 80px;
       text-align: center;
     }
-   
   }
-
-  
-
-`
+`;
